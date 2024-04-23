47887f12e6d593a129e0ff59019f98ad0ddbb374
ZorEl212 <yabsirad212@proton.me>
2024-04-24 01:34:18 +0300

    Add concurrent coroutines implementation
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Concurrent coroutines example"""
+import asyncio
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+async def wait_n(n: int, max_delay: int) -> float:
+    """Wait for a random delay between 0 and max_delay"""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
