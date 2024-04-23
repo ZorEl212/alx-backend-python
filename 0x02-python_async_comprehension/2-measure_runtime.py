bf04279351294e5bb2a6a48fdc990b9cc43be970
ZorEl212 <yabsirad212@proton.me>
2024-04-24 01:37:31 +0300

    Add measure_runtime function to calculate the runtime in 2-measure_runtime.py
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Measure runtime"""
+import asyncio
+import random
+import time
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """Measure runtime"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end = time.time()
+    return end - start
